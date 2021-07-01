7fb4c6ead76ffa667ad9445e64e50e22c744c502
aquillen <alice.quillen@gmail.com>
2021-07-01 07:07:19 -0400

    more runs and some bYORP figs

--- myexamples/pylab/cp.bat
+++ myexamples/pylab/cp.bat
@@ -1,2 +1,7 @@
 # from bin_try3.ipynb
 /bin/cp a1.png ../../../tex_binary/
+/bin/cp b1.png ../../../tex_binary/
+/bin/cp b1c.png ../../../tex_binary/
+/bin/cp tau_BY_obl.png ../../../tex_binary/
+/bin/cp tau_BY_obl2.png ../../../tex_binary/
+/bin/cp squannit.png ../../../tex_binary/
